599ab88fb1d42a327b3e487292d03598ed864feb
kna27 <krisharora27@gmail.com>
2021-08-03 13:03:36 -0400

    Create empty tank script

--- EmptyTank/EmptyTank/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace EmptyTank
-{
-    public class Class1
-    {
-    }
-}
--- EmptyTank/EmptyTank/EmptyTank.cs
+++ EmptyTank/EmptyTank/EmptyTank.cs
@@ -1,7 +1,32 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
 namespace EmptyTank
 {
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
     public class EmptyTank
     {
-        
+        private static List<Part> vessel;
+        public static void EmptyTanks()
+        {
+            ScreenMessages.PostScreenMessage("emptied");
+
+            foreach (Part part in vessel)
+            {
+                Console.WriteLine(part);
+                ScreenMessages.PostScreenMessage(part);
+            }
+        }
+
+        public static void FillTanks()
+        {
+            ScreenMessages.PostScreenMessage("filled");
+        }
+
+        void Update()
+        {
+            vessel = EditorLogic.SortedShipList;
+        }
     }
 }
